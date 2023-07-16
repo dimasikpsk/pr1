b9fe3564c4fdbe1c2669013370b7c074db37cb34
dimasikpsk <139615397+dimasikpsk@users.noreply.github.com>
2023-07-16 21:14:50 +0300

    Create cal.js

---
+++ cal.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Калькулятор</title>
+</head>
+<body>
+	<!-- поле ввода первого числа -->
+	<input id="num1" />
+
+	<!-- блок с кнопками -->
+	<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+
+	<!-- поле ввода второго числа -->
+	<input id="num2" />
+	<br>
+
+	<!-- кнопка для рассчётов -->
+	<button >Посчитать</button>
+
+</body>
+</html>
