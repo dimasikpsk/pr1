203f92cd700c74974c6ded93c18b69dc3fbb72de
dimasikpsk <139615397+dimasikpsk@users.noreply.github.com>
2024-03-18 14:44:31 +0300

    Update cal.js

--- cal.js
+++ cal.js
@@ -24,6 +24,106 @@ xscdxcscsczc  sc
 	</div>
 	<!-- поле ввода второго числа -->
 	<input id="num2" />
+	<br>  
+	<!-- поле ввода первого числа -->
+	<input id="num1" />
+xscdxcscsczc  sc
+	
+	<!-- блок с   кнопками -->
+	<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+	<!-- поле ввода второго числа -->
+	<input id="num2" />
+	<br>  
+	<!-- поле ввода первого числа -->
+	<input id="num1" />
+xscdxcscsczc  sc
+	
+	<!-- блок с   кнопками -->
+	<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+	<!-- поле ввода второго числа -->
+	<input id="num2" />
+	<br>  
+	<!-- поле ввода первого числа -->
+	<input id="num1" />
+xscdxcscsczc  sc
+	
+	<!-- блок с   кнопками -->
+	<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+	<!-- поле ввода второго числа -->
+	<input id="num2" />
+	<br>  
+	<!-- поле ввода первого числа -->
+	<input id="num1" />
+xscdxcscsczc  sc
+	
+	<!-- блок с   кнопками -->
+	<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+	<!-- поле ввода второго числа -->
+	<input id="num2" />
+	<br>     
+	<!-- поле ввода первого числа -->
+	<input id="num1" />
+xscdxcscsczc  sc
+	
+	<!-- блок с   кнопками -->
+	<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+<div id="operator_btns">
+	  <button id="plus" >+</button>
+	  <button id="minus" >-</button>
+	  <button id="times" >x</button>
+	  <button id="divide" >:</button>
+	</div>
+	<!-- поле ввода второго числа -->
+	<input id="num2" />
 	<br>
 
 	<!-- кнопка для рассчётов -->
