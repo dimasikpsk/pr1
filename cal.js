d31253472bce0fc5f1670dc731d696ce0a7a97f4
dimasikpsk <139615397+dimasikpsk@users.noreply.github.com>
2024-09-16 10:02:31 +0300

    Update cal.js

--- cal.js
+++ cal.js
@@ -103,6 +103,39 @@ xscdxcscsczc  sc
 <body>  
 	<!-- поле ввода первого числа -->
 	<input id="num1" />
+xscdxcscsczc  sc<!-- блок с   кнопками -->
+	<div id="opdghjorgijwpegdc
+	
+	csgergrrrgerator_btns">
+	  <button id="plus" >+</button>
+	  <button смииачимваимми
+	авиа
+		авиваиcdgerbbrebhhbdvseHJ247KYJead>
+<body>  
+	<!-- поле ввода первого числа -->
+	<input id="num1" />
+xscdxcscsczc  sc<!-- блок с   кнопками -->
+	<div id="opdghjorgijwpegdc
+	
+	csgergrrrgerator_btns">
+	  <button id="plus" >+</button>
+	  <button смииачимваимми
+	авиа
+		авиваиcdgerbbrebhhbdvseHJ247KYJead>
+<body>  
+	<!-- поле ввода первого числа -->
+	<input id="num1" />
+xscdxcscsczc  sc<!-- блок с   кнопками -->
+	<div id="opdghjorgijwpegdc
+	
+	csgergrrrgerator_btns">
+	  <button id="plus" >+</button>
+	  <button смииачимваимми
+	авиа
+		авиваиcdgerbbrebhhbdvseHJ247KYJead>
+<body>  
+	<!-- поле ввода первого числа -->
+	<input id="num1" />
 xscdxcscsczc  sc
 	
 	<!-- блок с   кнопками -->
